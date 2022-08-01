cd6d4fbe1ec57c60a676f068a0203634265ac3dd
Kingsley <long.kingsley@gmail.com>
2022-08-01 10:47:23 +0100

    upload site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Kingsley's space!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/2021/02/11/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Deep Clean Your Mac For Free, Say Goodbye To Cleanmymac",
+        "excerpt":"跟大家分享一下如何深度清理Mac的文件 How to clean my Mac *Please note that this article uses some terminal knowledge. If you are familiar with *Linux, then it won’t be a problem. $ sudo du -sh /System/Library/* | sort -h 500M Extensions 596M Fonts 639M LinguisticData 854M Templates 878M CoreServices 918M Caches 1018M Frameworks 1.1G...","categories": [],
+        "tags": [],
+        "url": "/2021/02/12/Deep-clean-your-Mac-for-free,-say-goodbye-to-CleanMyMac.html",
+        "teaser": "/assets/images/123.png"
+      }]
